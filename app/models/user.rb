d6da9efc7a17d8d7568466cedc2a4eb66320b659
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-04 01:14:58 +0900

    feat: user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, presnece: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+    format: {
+      with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
+    }
+
   has_secure_password
 end
