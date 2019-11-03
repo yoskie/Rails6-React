1f9a0020cbfcf505a61473909b1620a35c93420b
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-03 23:23:33 +0900

    feat: model validation added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, numericality: {
+    greater_than_equal_to: 0.0
+  }
 end
