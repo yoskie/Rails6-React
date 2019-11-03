6ba3169f896fd878a8f154bf35e87db339ffe505
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-03 23:47:17 +0900

    fix: flash setting added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
       flash[:notice] = "Product has been saved"
       redirect_to root_path
     else
-      flash[:alart] = "Product has not been saved"
+      flash.now[:alart] = "Product has not been saved"
       render :new
     end
   end
@@ -31,7 +31,7 @@ class ProductsController < ApplicationController
       flash[:notice] = "Product has been updated"
       redirect_to root_path
     else
-      flash[:alert] = "Product has not been updated"
+      flash.now[:alert] = "Product has not been updated"
       render :edit
     end
   end
