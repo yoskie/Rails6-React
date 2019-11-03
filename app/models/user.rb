feba0bc09de37229bf4b666f1ebbb02704319f75
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-04 01:19:04 +0900

    feat: callback added for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, presnece: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
-    format: {
-      with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
-    }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: {
+              with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
+            }
 
   has_secure_password
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
