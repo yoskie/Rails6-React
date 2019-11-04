4e8184bc4f65df3cd61c088d2c3c9995054326ea
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-05 04:46:29 +0900

    feat: signin filter added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :require_signin, execpt: [:index, :show]
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
