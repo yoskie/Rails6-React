7b592a4e10d643c00e567e377b53a25b5b7f8765
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-04 17:52:31 +0900

    feat: sessions controller and view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/new', to: 'users#new', as: 'new_user'
   get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy', as: 'session'
+
   resources :users, only: [:create]
 
   root to: "products#index"
