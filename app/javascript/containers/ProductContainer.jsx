9e3d2bea51474cfcd670b492d17517fcda3c0fb6
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2019-11-08 05:32:56 +0900

    feat: lifecycle method implemented

--- app/javascript/containers/ProductContainer.jsx
+++ app/javascript/containers/ProductContainer.jsx
@@ -1,26 +1,55 @@
 import React from "react";
+import axios from 'axios'
 
 import Product from '../components/products/Product'
 
-const ProductList = () => {
-  const products = ['Product1', 'Product2', 'Product3']
-  const productList = products.map((product) => <Product key={product} />)
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 mb-2">
-          <div className="row">
-            <div className="card-deck">
+    this.state = {
+      products: []
+    }
+  }
 
-              { productList }
+  componentDidMount() {
+    this.loadProductsFromServer()
+  }
 
+  loadProductsFromServer() {
+    axios
+      .get('/api/v1/products.json')
+      .then(response => {
+        const { products } = response.data
+        this.setState({ products })
+      })
+      .catch(error => console.log(error.response.data))
+  }
+
+  render () {
+    const { products } = this.state
+    const productList = products.map(
+      (product) => <Product key={product.id} />
+    )
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 mb-2">
+            <div className="row">
+              <div className="card-deck">
+
+                { productList }
+
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+
 
 }
 
